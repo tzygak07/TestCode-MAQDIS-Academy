1cd094e2fd41607367eec977820ea44e206d0c1c
farrel raza <lendrafarrel@gmail.com>
2026-05-05 21:58:16 +0700

    fixing mobile mode

--- app/tugas-2/components/Topbar.tsx
+++ app/tugas-2/components/Topbar.tsx
@@ -15,7 +15,7 @@ export default function Topbar({ onMenuToggle }: TopbarProps) {
         </div>
       </div>
 
-      <div className="flex items-center gap-3 pl-4 border-l border-[#edeeef] max-md:hidden">
+      <div className="flex items-center gap-3 pl-4 border-l border-[#edeeef]">
         <div className="w-9 h-9 rounded-full bg-[#ff8a3d]/20 border border-[#edeeef] flex items-center justify-center text-[#9a4600]">
           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
         </div>
